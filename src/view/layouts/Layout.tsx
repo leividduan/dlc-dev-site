b071592794cdcb114e1fab2d889c183c6bf7fe28
Deivid <deivid.cardoso@weduka.com.br>
2023-10-03 22:57:29 -0300

    fix: tailwind class

--- src/view/layouts/Layout.tsx
+++ src/view/layouts/Layout.tsx
@@ -6,7 +6,7 @@ export function Layout() {
   return (
     <>
       <Header />
-      <div className="max-w-screen-xl mx-aut grow flex flex-col">
+      <div className="max-w-screen-xl mx-auto grow flex flex-col">
         <div className="w-full mx-auto p-4">
           <Outlet />
         </div>
